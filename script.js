b11f11a81135833222e59b95f8bb6333cf395e75
ZeSinitry <maximecorraza@yahoo.fr>
2023-08-31 11:57:06 +0200

    Modification JS

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-let mot = prompt("Ecrivez Oui")
+// Déclaration du tableau contenant la liste des mots proposés à l'utilisateur
+const listeMots = ['Cachalot', 'Pétunia', 'Serviette']
+const listePhrases = ['Pas de panique !', 'Je suis la', 'Tu vas bien ?']
 
-while (mot !== "Oui") {
-    mot = prompt("Ecrivez Oui")
+let score = 0
+
+let choix = prompt("Choississez : Mots ou Phrases")
+while (choix !== "Mots" && choix !== "Phrases"){
+    choix = prompt("Choississez : Mots ou Phrases")
 }
 
-console.log("Bravo")
+if (choix === "Mots") {
+    for (let i = 0; i < listeMots.length; i++) {
+        let motUtilisateur = prompt('Entrez le mot : ' + listeMots[i])
+        if (motUtilisateur === listeMots[i]) {
+            score++
+        }
+        console.log("Votre score est de " + score + " sur " + listeMots.length)
+    }
+} else {
+    for (let i = 0; i < listePhrases.length; i++) {
+        let motUtilisateur = prompt('Entrez le mot : ' + listePhrases[i])
+        if (motUtilisateur === listePhrases[i]) {
+            score++
+        }
+    }
+    console.log("Votre score est de " + score + " sur " + listePhrases.length)
+}
